75903f4ff36ac7adcc00af7b15919bf6f31c8333
TIMESGROUP\Himanshu.Verma2 <himanshu.verma2@magicbricks.com>
2024-08-04 22:21:46 +0530

    removed milestone from project

--- milestoneSimple/src/main/java/com/example/milestone/repo/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.milestone.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.milestone.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
